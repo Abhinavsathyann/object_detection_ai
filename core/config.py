06949cde801482126f1b3aa3d148182c42880598
Abhinav K <abhinavksathyann@gmail.com>
2025-09-27 20:10:18 +0530

    modified:   core/config.py

--- core/config.py
+++ core/config.py
@@ -1,11 +1,5 @@
-# For full YOLOv3
-MODEL_CFG = "models/yolov3.cfg"
-MODEL_WEIGHTS = "models/yolov3.weights"
-
-# Or for tiny (faster)
-# MODEL_CFG = "models/yolov3-tiny.cfg"
-# MODEL_WEIGHTS = "models/yolov3-tiny.weights"
-
+MODEL_CFG = "models/yolov3-tiny.cfg"
+MODEL_WEIGHTS = "models/yolov3-tiny.weights"
 MODEL_NAMES = "models/coco.names"
 CONF_THRESHOLD = 0.5
 NMS_THRESHOLD = 0.4
